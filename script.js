373a02766c9ba07df5c413142c0a969765138e5a
Nahum <nahumvelazquez24@gmail.com>
2026-02-17 14:33:53 -0300

    Mas mejoras

--- script.js
+++ script.js
@@ -2458,6 +2458,32 @@ class ANDEDashboard {
             dataPoints: this.data.length,
             periodRange: this.getPeriodRange(),
             palette: this.chartPalette
+            const seriesData = {};
+this.data.forEach(d => {
+    if (!seriesData[d.combinationKey]) seriesData[d.combinationKey] = [];
+    seriesData[d.combinationKey].push(d.frecuencia);
+});
+
+const chartData = {
+    // ... tus propiedades existentes ...
+    mainChart: { /* ... */ },
+    rankingChart: { /* ... */ },
+    rankingChartAll: { /* ... */ },
+    scatterChart: { /* ... */ },
+    pieFeeder: { /* ... */ },
+    pieType: { /* ... */ },
+    stationSummary: { /* ... */ },
+    stationSummaryBundle: this.expandedStationSummaryBundle || this.getStationSummaryBundle(this.getCompleteSelectedStations()),
+    stationSummarySelection: this.stationSummarySelection || '__ALL__',
+    seriesCount: this.mainChart?.data.datasets?.length || 0,
+    dataPoints: this.data.length,
+    periodRange: this.getPeriodRange(),
+    palette: this.chartPalette,
+    rankingFullData: { series: seriesData }  // <--- NUEVO
+};
+
+localStorage.setItem('ande_chart_data', JSON.stringify(chartData));
+window.open('chart.html', '_blank', 'width=1400,height=900');
         };
         localStorage.setItem('ande_chart_data', JSON.stringify(chartData));
         window.open('chart.html', '_blank', 'width=1400,height=900');
